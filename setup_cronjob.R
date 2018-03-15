5676839f92bffff0eaddb44327b4ca75753c4b87
Saskia Freytag <freytag.s@unix401.wehi.edu.au>
2018-03-15 11:49:57 +1100

    Managed to get cronjob to work, screenshot of the great work

--- setup_cronjob.R
+++ setup_cronjob.R
@@ -3,45 +3,28 @@ library(RCurl)
 library(ROAuth)
 library(RJSONIO)
 library(devtools)
-library(rtweet)
+library(twitteR)
 
 # Cronjob to download Royal Tweets
-
+options(httr_oob_default = TRUE)
 setwd("~/RLadies/Royal-Twitter-Workshop-useR-2018/")
 
 # Download tweets
 Hashtags <- read_csv("Hashtags.csv", col_names = FALSE)
 
-## name of app you created
-appname <- "SaskiaFreytag"
-
-## api consumer
-key <- 'O3G4prQHtVY8mV6XH0BbDFqwy'
-
-## api comsumer secret 
-secret <- "nGqAZNnPaqqztuGlQnVYvOoLfmXjuZssMu4JhG2SZiKnQsDhqA"
-
-## create a token, storing it as object 'twitter_token'
-twitter_token <- create_token(
-  app = appname,
-  consumer_key = key,
-  consumer_secret = secret,set_renv=FALSE)
+api_key<-'O3G4prQHtVY8mV6XH0BbDFqwy'
+api_secret<- "nGqAZNnPaqqztuGlQnVYvOoLfmXjuZssMu4JhG2SZiKnQsDhqA"
+token<-"766200832140468224-5PUgnHme5ftycJasL9SPxJbVgiFz9Ex"
+token_secret<- "MGqeEdHIrgc9xPD8nyjSjMvbwfBIcKsuJFsdDHlJlbO9o"
+setup_twitter_oauth(api_key, api_secret, token, token_secret)
 
 # Search tweets
-rla_tweet <- search_tweets(paste0(Hashtags$X1,collapse=" OR ") , n = 3000, token = twitter_token, lang='en')
+rla_tweet <- searchTwitter(paste0(Hashtags$X1,collapse=" OR "), 
+                           n=3000, lang="en")
 
 # recode lists to vectors to save it into CSV
-names_of_lists <- names(sapply(rla_tweet,typeof))[which(sapply(rla_tweet,typeof) %in% "list")]
+rla_tweet <- twitteR::twListToDF(rla_tweet)
 
-list_to_vector <- function(recode_list){
-  sapply(recode_list,
-         function(entry_list) {paste(entry_list,collapse=",")})
-}
-
-z <- rla_tweet %>%
-  mutate_at(.vars = vars(names_of_lists), 
-            .funs = funs(list_to_vector))
-
-write.csv(z, paste0("Twitter_Data/", Sys.time(), ".csv"))
+write.csv(rla_tweet, paste0("Twitter_Data/", Sys.Date(), ".csv"))
 
 
